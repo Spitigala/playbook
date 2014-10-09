cbfd6a45956e7a07bbff4578004b66061c457990
Sahan <keeblersf@gmail.com>
2014-10-09 17:06:10 -0400

    Add created/updated at fields for player model

--- players/models.py
+++ players/models.py
@@ -7,6 +7,8 @@ class Player(models.Model):
     sport = models.CharField(max_length=20)
     league = models.CharField(max_length=100)
     team = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
         return '%s %s %s %s %s' % (self.first_name, self.last_name, self.sport, self.league, self.team)
