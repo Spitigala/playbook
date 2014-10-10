4abad3af332d2f395ceb5270073d118950307eb5
Sahan <keeblersf@gmail.com>
2014-10-10 13:54:36 -0400

    add edit and delete functionality. Clean up file

--- players/views.py
+++ players/views.py
@@ -6,29 +6,12 @@ from django.template import loader, Context
 from forms import PlayerForm
 from django.http import HttpResponseRedirect
 from django.core.context_processors import csrf
-# from someapp.forms import StudentForm
 
 def players(request):
     all_players = Player.objects.all().order_by('-last_name')
-    # context = {"all_players": all_players}
-    # return render(request, 'players/index.html', context)
     t = loader.get_template('players/index.html')
     c = Context({"all_players": all_players})
     return HttpResponse(t.render(c))
-
-# def players_template(request):
-#     all_players = Player.objects.all().order_by('-last_name')
-#     context = {"all_players": all_players}
-#     return render(request, 'players/index.html', context)
-
-# class PlayerTemplate(TemplateView):
-#     template_name = 'player_class.html'
-
-#     def get_context_data(self, **kwargs):
-#         context = super(PlayerTemplate, self).get_context_data(**kwargs)
-#         all_players = Player.objects.all().order_by('-last_name')
-#         context = {"all_players": all_players}
-#         return context
 
 def create(request):
     if request.POST:
@@ -43,3 +26,25 @@ def create(request):
     args.update(csrf(request))
     args['form'] = form
     return render_to_response('players/create.html', args)
+
+def edit(request, player_id):
+    if player_id:
+        p = Player.objects.get(id=player_id)
+    if request.method == "POST":
+        form = PlayerForm(request.POST, instance=p)
+        if form.is_valid():
+        	  form.save()
+        	  return HttpResponseRedirect('/players/')
+    else:
+    	  form = PlayerForm(instance=p)
+
+    args = {}
+    args.update(csrf(request))
+    args['form'] = form
+    args['player'] = p
+    return render_to_response('players/edit.html', args)
+
+def delete(request, player_id):
+    p = Player.objects.get(id=player_id)
+    p.delete()
+    return HttpResponseRedirect('/players/')
