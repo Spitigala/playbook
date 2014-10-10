5533d27baa5ee4e390e69244997af9be6e042370
Sahan <keeblersf@gmail.com>
2014-10-10 09:38:35 -0400

    Add urls for viewing all players, creating player

--- players/urls.py
+++ players/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-
 from players import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
     url(r'^$', views.players, name='players'),
+    url(r'^create/$', views.create, name='create'),
+    # url(r'^create/$', views.create, name='create'),
+    # url(r'^$', views.index, name='index'),
 ]
