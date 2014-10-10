48d2711a988ccacd98ae1161316f648590439717
Sahan <keeblersf@gmail.com>
2014-10-10 09:37:58 -0400

    Add basic url handlers

--- playbook/urls.py
+++ playbook/urls.py
@@ -4,10 +4,11 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'playbook.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', include('players.urls')),
-    url(r'^players/', include('players.urls')),
+    # url(r'^$', include('players.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^players/', include('players.urls')),
     # url(r'^players/', 'players.views.players'),
 )
+
+# for root url, you can't handle it through player.
