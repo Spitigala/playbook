0ccfe7b283210d5a732eb50b0ac8e0e6df76b8dd
Sahan <keeblersf@gmail.com>
2014-10-10 13:52:21 -0400

    Clean up url file

--- playbook/urls.py
+++ playbook/urls.py
@@ -2,13 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'playbook.views.home', name='home'),
-
-    # url(r'^$', include('players.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^players/', include('players.urls')),
-    # url(r'^players/', 'players.views.players'),
 )
 
 # for root url, you can't handle it through player.
