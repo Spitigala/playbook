1c9cde557288fc00c4201886c6d975a6c70f6ec3
Sahan <keeblersf@gmail.com>
2014-10-10 13:54:14 -0400

    Add urls for edit and delete, clean up file

--- players/urls.py
+++ players/urls.py
@@ -4,6 +4,6 @@ from players import views
 urlpatterns = [
     url(r'^$', views.players, name='players'),
     url(r'^create/$', views.create, name='create'),
-    # url(r'^create/$', views.create, name='create'),
-    # url(r'^$', views.index, name='index'),
+    url(r'^edit/(?P<player_id>\d+)/$', views.edit, name='edit'),
+    url(r'^delete/(?P<player_id>\d+)/$', views.delete, name='delete'),
 ]
