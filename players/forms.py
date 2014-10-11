13a417b87c2e2628765405e403486384f715018d
Sahan <keeblersf@gmail.com>
2014-10-11 12:22:00 -0400

    add classes to form elements for styling

--- players/forms.py
+++ players/forms.py
@@ -5,3 +5,10 @@ class PlayerForm(forms.ModelForm):
 
     class Meta:
         model = Player
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'sport': forms.TextInput(attrs={'class': 'form-control'}),
+            'league': forms.TextInput(attrs={'class': 'form-control'}),
+            'team': forms.TextInput(attrs={'class': 'form-control'}),
+        }
