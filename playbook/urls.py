08a8d9c7f94c870bcc0820a8f1353a917631bdd3
Sahan <keeblersf@gmail.com>
2014-10-11 17:36:53 -0400

    configure urls file for deployment

--- playbook/urls.py
+++ playbook/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+import settings
+
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('players.urls')),
 )
+
+if not settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+    urlpatterns += staticfiles_urlpatterns()
