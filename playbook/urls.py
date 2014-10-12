3c61ce6d82b18b3341c7655220ecc6c1bd1009c6
Sahan <keeblersf@gmail.com>
2014-10-11 22:00:06 -0400

    Add haystack url handler for search

--- playbook/urls.py
+++ playbook/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+import haystack
 
 import settings
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^search/', include('haystack.urls')),
     url(r'^', include('players.urls')),
 )
 
